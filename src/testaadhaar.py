f76a59c4ea6328f48b76533b360f1639ffe0bee2
dilippuri <dilippuripuri@gmail.com>
2016-07-04 18:59:44 +0530

    some changes in code

--- src/testaadhaar.py
+++ src/testaadhaar.py
@@ -9,6 +9,7 @@ import re
 import difflib
 import csv
 import nltk
+import subprocess
 import dateutil.parser as dparser
 from dateutil.parser import _timelex, parser
 from PIL import Image, ImageEnhance, ImageFilter
@@ -37,7 +38,11 @@ img.crop((e,f,e1,f1)).save('img3.jpg')
 texttest = pytesseract.image_to_string(Image.open('img3.jpg'))
 print(texttest)
 #'''
-text = pytesseract.image_to_string(Image.open('temp.jpg'))
+#text = pytesseract.image_to_string(Image.open('temp.jpg'))
+subprocess.call("tesseract temp.jpg out", shell=True)
+fi = open('out.txt', 'r')
+text = fi.read()
+
 text = filter(lambda x: ord(x)<128,text)
 
 
@@ -76,13 +81,14 @@ except:
 	pass
 
 try:
-	yearline = re.split('Year|Birth|irth|YoB|YOB:|DOB:|DOB', yearline)[1:]
+	yearline = re.split('Year|Birth|Birth |Birth :|Birth:|irth|YoB|YOB:|DOB:|DOB', yearline)[1:]
 	yearline = ''.join(str(e) for e in yearline)
 	if(yearline):
 		ayear = dparser.parse(yearline,fuzzy=True).year
 except:
 	pass
 #------------------------------------------------------	
+'''
 try:
 	p = parser()
 	info = p.info
@@ -114,6 +120,7 @@ try:
 	  print "Parsed:", p.parse(item)
 except:
 	pass
+#'''
 #-----------------------------------------------------------
 
 # Searching for Gender
